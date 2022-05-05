786e6e78c5adacc6a78d252e4ca9fcbe52798000
Kenyon Hensler <kenyon.j.hensler@gmail.com>
2022-05-05 12:39:49 -0400

    nsx 3.1.2 fixes

--- main.py
+++ main.py
@@ -29,15 +29,15 @@ class NSXTTier0Interface:
         self.NSXTier0 = T0
         self.connection= T0.connection
     def getInterfaceStats(self):
-        return getInterfaceStats(self)
+        return _getInterfaceStats(self)
 
 class NSXTTier0:
     def __init__(self, path, name, connection):
         self.path = path
         self.name = name
         self.connection = connection
-        self.local_services = getT0LocaleServices(self.connection,self.path)
-        self.interfaces = getT0Interfaces(self)
+        self.local_services = _getT0LocaleServices(self.connection,self.path)
+        self.interfaces = _getT0Interfaces(self)
     def getInterfacesStats(self):
         interfaceStats = []
         for interface in self.interfaces:
@@ -50,46 +50,50 @@ class NSXEdgeNode:
         self.display_name = display_name
         self.connection = connection
     def getCPUStats(self):
-        nodecpu = json.loads(getAPIResults(nsxtConnection=self.connection,uri= "/api/v1/transport-nodes/"+self.id+"/node/services/dataplane/cpu-stats",policy=False))
+        nodecpu = json.loads(_getAPIResults(nsxtConnection=self.connection,uri= "/api/v1/transport-nodes/"+self.id+"/node/services/dataplane/cpu-stats",policy=False))
         df = pandas.json_normalize(nodecpu, record_path=['cores'])
         df.columns = df.columns.str.replace('/','_',regex=False)
         df['precise_timestamp'] = datetime.datetime.now()
         df['t0_name'] = self.display_name
         return df
 
-def getT0Interfaces(NSXTTier0):
+def _getT0Interfaces(NSXTTier0):
     interfaces = []
-    results = json.loads(getAPIResults(nsxtConnection=NSXTTier0.connection,uri= NSXTTier0.local_services+"/interfaces"))
+    results = json.loads(_getAPIResults(nsxtConnection=NSXTTier0.connection,uri= NSXTTier0.local_services+"/interfaces"))
     for tier0interface in results['results']:
         interfaces.append(NSXTTier0Interface(path = tier0interface['path'], edge_path=tier0interface['edge_path'],T0= NSXTTier0))
     return interfaces
     
-def getT0(nsxtConnection, id):
-    results = json.loads(getAPIResults(nsxtConnection=nsxtConnection, uri="/infra/tier-0s/"+id))
+def _getT0(nsxtConnection, id):
+    results = json.loads(_getAPIResults(nsxtConnection=nsxtConnection, uri="/infra/tier-0s/"+id))
     return NSXTTier0(path = results['path'], name = results['display_name'], connection=nsxtConnection)
 
 def getT0s(nsxtConnection):
     T0s = []
-    results = json.loads(getAPIResults(nsxtConnection=nsxtConnection, uri="/infra/tier-0s"))
+    results = json.loads(_getAPIResults(nsxtConnection=nsxtConnection, uri="/infra/tier-0s"))
     for t0 in results['results']:
-        T0s.append(getT0(nsxtConnection=nsxtConnection, id=t0['display_name']))
+        T0s.append(_getT0(nsxtConnection=nsxtConnection, id=t0['display_name']))
     return T0s
 
-def getT0LocaleServices(nsxtConnection,path):
-    tier0locales = json.loads(getAPIResults(nsxtConnection=nsxtConnection, uri=path+"/locale-services"))  
+def _getT0LocaleServices(nsxtConnection,path):
+    tier0locales = json.loads(_getAPIResults(nsxtConnection=nsxtConnection, uri=path+"/locale-services"))  
     return tier0locales['results'][0]['path']
 
 def getInterfacesStats(tier0interfaces):
     frames =[]
     for tier0interface in tier0interfaces:
-        interfaceStats = getInterfaceStats(tier0interface)
+        interfaceStats = _getInterfaceStats(tier0interface)
         frames.append(interfaceStats)
     return pandas.concat(frames)
 
-def getInterfaceStats(tier0interface):
+def _getInterfaceStats(tier0interface):
     statsuri = tier0interface.path+"/statistics?enforcement_point_path=/infra/sites/default/enforcement-points/default&edge_path="+tier0interface.edge_path
-    tier0interfacestats = json.loads(getAPIResults(nsxtConnection=tier0interface.connection, uri=statsuri))
+    tier0interfacestats = json.loads(_getAPIResults(nsxtConnection=tier0interface.connection, uri=statsuri))
     df = pandas.json_normalize(tier0interfacestats, record_path=['per_node_statistics'])
+    #df = pandas.json_normalize(tier0interfacestats, record_path=['per_node_statistics'], meta='logical_router_port_id')
+    #cols = df.columns.tolist()
+    #cols = cols[-1:] + cols[:-1]
+    #df = df[cols]
     df.columns = df.columns.str.replace('.','_',regex=False)
     df['precise_timestamp'] = datetime.datetime.fromtimestamp(int(tier0interfacestats['per_node_statistics'][0]['last_update_timestamp'])/1000,tz=datetime.timezone.utc)
     df['t0_name'] = tier0interface.NSXTier0.name
@@ -98,7 +102,7 @@ def getInterfaceStats(tier0interface):
 
 def getEdgeNodes(nsxtConnection):
     nodes = []
-    results = json.loads(getAPIResults(nsxtConnection=nsxtConnection, uri="/api/v1/transport-nodes?node_types=EdgeNode", policy=False))['results']
+    results = json.loads(_getAPIResults(nsxtConnection=nsxtConnection, uri="/api/v1/transport-nodes?node_types=EdgeNode", policy=False))['results']
     for node in results:
         nodes.append(NSXEdgeNode(id = node['node_id'],display_name = node['display_name'], connection=nsxtConnection))
     return nodes
@@ -109,7 +113,7 @@ def getCpuStats(nodes):
         frames.append(node.getCPUStats())
     return pandas.concat(frames)
 
-def getAPIResults(nsxtConnection, uri,json_body=None, policy = True):
+def _getAPIResults(nsxtConnection, uri,json_body=None, policy = True):
     if (policy == True):
         r = requests.request(method="GET",url=nsxtConnection.nsxtPolicyUri+uri ,json=json_body,auth = requests.auth.HTTPBasicAuth (nsxtConnection.nsxtUsername, nsxtConnection.nsxtPassword), verify=False)
     else:
