b7c1656133d1337ae031b9592377220c4dc370b8
Kenyon Hensler <kenyon.j.hensler@gmail.com>
2022-11-03 10:08:52 -0400

    changes to install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-while getopts ":p:" opt; do
+while getopts ":p:c:" opt; do
   case $opt in
     p) install_path="$OPTARG"
     ;;
@@ -17,12 +17,11 @@ while getopts ":p:" opt; do
   esac
 done
 
-apt-get update
 wget -qO- https://repos.influxdata.com/influxdb.key | tee /etc/apt/trusted.gpg.d/influxdb.asc >/dev/null
 source /etc/os-release && echo "deb https://repos.influxdata.com/${ID} ${VERSION_CODENAME} stable" | tee /etc/apt/sources.list.d/influxdb.list
 apt-get update && apt-get install telegraf
 service telegraf stop
-echo AVS_CLOUD_ID=$cloud_id > /$install_path/.env
+echo AVS_CLOUD_ID=$cloud_id > $install_path/.env
 mkdir $install_path
 cp main.py $install_path
 cp requirements.txt $install_path
@@ -31,9 +30,10 @@ cd $install_path
 python3 -m venv venv
 source ./venv/bin/activate
 python3 -m pip install -r requirements
-sed -i "s/##WORKINGDIR##/$install_path/" nsx-stat.service
+sed -i "s~/##WORKINGDIR##~$install_path~" nsx-stat.service
 cp nsx-stat.service /etc/systemd/system/
 systemctl daemon-reload
-sed -i "s/##WORKINGDIR##/$install_path/" telegraf.conf
+sed -i "s~/##WORKINGDIR##~$install_path~" $install_path/telegraf.conf
+cp $install_path/telegraf.conf /etc/telegraf/
 systemctl start telegraf
 systemctl start nsx-stats
